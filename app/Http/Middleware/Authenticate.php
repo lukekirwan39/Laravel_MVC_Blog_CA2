7140efb082c4cfec7b76904a5b17509d0f2eab95
lukekirwan39 <lukekirwan39@gmail.com>
2025-03-21 09:08:53 +0000

    Updated Login and Logout form completed with email and username login

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,13 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return route('login');
+//            return route('login');
+            if ($request->routeIs('author.*')) {
+                session()->flash('fail', 'You must be logged in first');
+                return route('author.login');
+            } else {
+                return route('login');
+            }
         }
     }
 }
