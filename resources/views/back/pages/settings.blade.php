35ed847c844632c1f98f3640cb2f222e11b5e565
lukekirwan39 <lukekirwan39@gmail.com>
2025-03-25 09:24:23 +0000

    Social networks finished

--- resources/views/back/pages/settings.blade.php
+++ resources/views/back/pages/settings.blade.php
@@ -4,16 +4,6 @@
     <div class="panel">
         <div class="mb-5 flex items-center justify-between">
             <h5 class="text-lg font-semibold dark:text-white-light">Settings</h5>
-            <a class="font-semibold hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-600" href="javascript:;" @click="toggleCode('code8')">
-                <span class="flex items-center">
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ltr:mr-2 rtl:ml-2">
-                        <path d="M17 7.82959L18.6965 9.35641C20.239 10.7447 21.0103 11.4389 21.0103 12.3296C21.0103 13.2203 20.239 13.9145 18.6965 15.3028L17 16.8296" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"></path>
-                        <path opacity="0.5" d="M13.9868 5L10.0132 19.8297" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"></path>
-                        <path d="M7.00005 7.82959L5.30358 9.35641C3.76102 10.7447 2.98975 11.4389 2.98975 12.3296C2.98975 13.2203 3.76102 13.9145 5.30358 15.3028L7.00005 16.8296" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"></path>
-                    </svg>
-                    Code
-                </span>
-            </a>
         </div>
         <div class="mb-5" x-data="{ tab: 'home'}">
             <div>
@@ -45,35 +35,7 @@
             </div>
             <div x-show="tab === 'contact'" x-cloak>
                 <div>
-                    <form action="">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="mb-3">
-                                    <label class="form-label">Facebook</label>
-                                    <input type="text" class="form-input" placeholder="Facebook page URL">
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="mb-3">
-                                    <label class="form-label">Instagram</label>
-                                    <input type="text" class="form-input" placeholder="Instagram URL">
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="mb-3">
-                                    <label class="form-label">YouTube</label>
-                                    <input type="text" class="form-input" placeholder="YouTube channel URL">
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="mb-3">
-                                    <label class="form-label">LinkedIn</label>
-                                    <input type="text" class="form-input" placeholder="LinkedIn URL">
-                                </div>
-                            </div>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Update</button>
-                    </form>
+                    @livewire('author-blog-social-media-form')
                 </div>
             </div>
         </div>
