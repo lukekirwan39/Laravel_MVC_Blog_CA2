0c11add0b5feefda160ae4276dab8a8d658e436d
lukekirwan39 <lukekirwan39@gmail.com>
2025-12-05 22:57:10 +0000

    feat: adding comments on single post page done

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\CommentController;
 
 use App\Http\Controllers\AuthorController;
 
@@ -15,3 +16,13 @@ Route::get('/article/{any}', [BlogController::class, 'readPost'])->name('read_po
 Route::get('/category/{any}', [BlogController::class, 'categoryPosts'])->name('category_posts');
 Route::get('/posts/tag/{any}', [BlogController::class, 'tagPosts'])->name('tag_posts');
 Route::get('/search', [BlogController::class, 'searchBlog'])->name('search_posts');
+Route::post('/posts/{post}/comments', [CommentController::class, 'store'])
+    ->name('comments.store');
+Route::get('/comments/{comment}/edit', [CommentController::class, 'edit'])
+    ->name('comments.edit');
+
+Route::put('/comments/{comment}', [CommentController::class, 'update'])
+    ->name('comments.update');
+
+Route::delete('/comments/{comment}', [CommentController::class, 'destroy'])
+    ->name('comments.destroy');
