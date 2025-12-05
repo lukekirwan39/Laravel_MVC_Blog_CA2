60e6b1d83fd6b483f70651d146af61fd714c4db7
lukekirwan39 <lukekirwan39@gmail.com>
2025-12-05 21:47:42 +0000

    feat: displaying a single post done, displaying related posts done and meta tags added as well

--- resources/views/front/pages/home.blade.php
+++ resources/views/front/pages/home.blade.php
@@ -120,13 +120,7 @@
                             <h2 class="section-title mb-3">Categories</h2>
                             <div class="widget-body">
                                 <ul class="widget-list">
-                                    @foreach(categories() as $item)
-                                    <li>
-                                        <a href="{{ route('category_posts',$item->slug) }}">{{ Str::ucfirst($item->subcategory_name) }}
-                                            <span class="ml-auto">({{ $item->posts->count() }})</span>
-                                        </a>
-                                    </li>
-                                    @endforeach
+                                    @include('front.layouts.inc.categories_list')
                                 </ul>
                             </div>
                         </div>
--- resources/views/front/pages/search_posts.blade.php
+++ resources/views/front/pages/search_posts.blade.php
@@ -4,34 +4,34 @@
 
     <div class="row">
         <div class="col-12">
-            <h1 class="mb-4 border-bottom border-primary d-inline-block">{{ $category->subcategory_name }}</h1>
+            <h1 class="mb-4 border-bottom border-primary d-inline-block">{{ $pageTitle }}</h1>
         </div>
         <div class="col-lg-8 mb-5 mb-lg-0">
             <div class="row">
                 @forelse($posts as $item)
 
-                <div class="col-md-6 mb-4">
-                    <article class="card article-card article-card-sm h-100">
-                        <a href="{{ route('read_post', $item->post_slug) }}">
-                            <div class="card-image">
-                                <div class="post-info"> <span class="text-uppercase">{{ date_formatter($item->created_at) }}</span>
-                                    <span class="text-uppercase">{{ readDuration($item->post_title, $item->post_content) }}
-                                        @choice('min|mins',readDuration($item->post_title, $item->post_content)) read</span>
+                    <div class="col-md-6 mb-4">
+                        <article class="card article-card article-card-sm h-100">
+                            <a href="{{ route('read_post', $item->post_slug) }}">
+                                <div class="card-image">
+                                    <div class="post-info"> <span class="text-uppercase">{{ date_formatter($item->created_at) }}</span>
+                                        <span class="text-uppercase">{{ readDuration($item->post_title, $item->post_content) }}
+                                            @choice('min|mins',readDuration($item->post_title, $item->post_content)) read</span>
+                                    </div>
+                                    <img loading="lazy" decoding="async" src="/storage/images/post_images/thumbnails/resized_{{ $item->featured_image }}"
+                                         alt="Post Thumbnail" class="w-100" width="420" height="280">
                                 </div>
-                                <img loading="lazy" decoding="async" src="/storage/images/post_images/thumbnails/resized_{{ $item->featured_image }}"
-                                     alt="Post Thumbnail" class="w-100" width="420" height="280">
+                            </a>
+                            <div class="card-body px-0 pb-0">
+                                <h2><a class="post-title" href="{{ route('read_post', $item->post_slug) }}">{{ $item->post_title }}</a></h2>
+                                <p class="card-text">{!! Str::ucfirst(words($item->post_content, 12)) !!}</p>
+                                <div class="content"> <a class="read-more-btn" href="{{ route('read_post', $item->post_slug) }}">Read Full Article</a>
+                                </div>
                             </div>
-                        </a>
-                        <div class="card-body px-0 pb-0">
-                            <h2><a class="post-title" href="{{ route('read_post', $item->post_slug) }}">{{ $item->post_title }}</a></h2>
-                            <p class="card-text">{!! Str::ucfirst(words($item->post_content, 12)) !!}</p>
-                            <div class="content"> <a class="read-more-btn" href="{{ route('read_post', $item->post_slug) }}">Read Full Article</a>
-                            </div>
-                        </div>
-                    </article>
-                </div>
+                        </article>
+                    </div>
                 @empty
-                    <span class="text-danger">No post(s) found for this category!</span>
+                    <span class="text-danger">No post(s) found!</span>
                 @endforelse
             </div>
 
@@ -107,16 +107,16 @@
                         </div>
                     </div>
                     @if(categories())
-                    <div class="col-lg-12 col-md-6">
-                        <div class="widget">
-                            <h2 class="section-title mb-3">Categories</h2>
-                            <div class="widget-body">
-                                <ul class="widget-list">
-                                    @include('front.layouts.inc.categories_list')
-                                </ul>
+                        <div class="col-lg-12 col-md-6">
+                            <div class="widget">
+                                <h2 class="section-title mb-3">Categories</h2>
+                                <div class="widget-body">
+                                    <ul class="widget-list">
+                                        @include('front.layouts.inc.categories_list')
+                                    </ul>
+                                </div>
                             </div>
                         </div>
-                    </div>
                     @endif
                 </div>
             </div>
