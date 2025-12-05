3d545f536da8776ccff974fa1f16676f835437ae
lukekirwan39 <lukekirwan39@gmail.com>
2025-12-05 22:23:20 +0000

    feat: displaying sidebar latest posts done

--- app/helpers.php
+++ app/helpers.php
@@ -110,3 +110,16 @@ if (!function_exists('categories')){
             ->get();
     }
 }
+
+/**
+ * SIDEBAR LATEST POSTS
+ */
+if (!function_exists('latest_sidebar_posts')){
+    function latest_sidebar_posts($except = null, $limit = 4) {
+        return Post::where('id','!=',$except)
+            ->limit($limit)
+            ->orderBy('created_at','desc')
+            ->get();
+
+    }
+}
