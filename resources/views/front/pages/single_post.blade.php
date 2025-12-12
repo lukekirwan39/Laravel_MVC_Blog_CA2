3e130251c5a70fdb823d5421d90180e11ff022f3
lukekirwan39 <lukekirwan39@gmail.com>
2025-12-12 18:02:48 +0000

    feat: displaying and paginating tag posts done

--- resources/views/front/pages/single_post.blade.php
+++ resources/views/front/pages/single_post.blade.php
@@ -174,17 +174,16 @@
                         $allTagsString = all_tags();
                         $allTagsArray = explode(',',$allTagsString);
                         @endphp
-                    <div>
-                        <div>
-                            <h2>
-                                <div>
-                                    <ul>
-                                        <li>
-
-                                        </li>
-                                    </ul>
-                                </div>
-                            </h2>
+                    <div class="col-lg-12 col-md-6">
+                        <div class="widget">
+                            <h2 class="section-title mb-3">Tags </h2>
+                            <div class="widget-body">
+                                <ul class="widget-list">
+                                    @foreach($allTagsArray as $tag)
+                                        <li><a href="{{ route('tag_posts', $tag) }}">#{{ $tag }}</a></li>
+                                    @endforeach
+                                </ul>
+                            </div>
                         </div>
                     </div>
                     @endif
